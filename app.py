e23273a6c80a1678944aeb7c88290e80cebf8950
Snigdha Janmanchi <jsaisigdha@gmail.com>
2025-06-12 23:03:18 -0400

    Update project files for deployment

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def upload_file():
         extracted_text = process_video(filepath)
         return render_template('result.html', transcription=extracted_text)
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
+
 
